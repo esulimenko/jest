5fc242bbdc53a35d370c6a61a86c1aba2a4e6da1
e.sulimenko <e.sulimenko@dunice.net>
2019-07-31 17:31:11 +0300

    Phoenix tests

--- index.test.js
+++ index.test.js
@@ -28,18 +28,66 @@ describe('Class Essence', () => {
     expect(() => { essence.isAlive = true }).toThrow();
   });
 
+  it('When age > maxAge isAlive turn to false', () => {
+    jest.useFakeTimers();
+    const essence = new Essence(1);
+    jest.runAllTimers();
+    expect(essence.isAlive).toBeFalsy();
+  });
+
   it('Recursive calling of growOld method until isAlive is false', () => {
     const essence = new Essence(3);
     const spyOnGrowOld = jest.spyOn(essence, 'growOld');
+    spyOnGrowOld.mockClear();
     // run all timers without delay
     jest.useFakeTimers();
-    // clear mock
-    spyOnGrowOld.mockClear();
     essence.growOld();
     // run all timers;
     jest.runAllTimers();
 
-    expect(spyOnGrowOld).toHaveBeenCalledTimes(3);
+    expect(spyOnGrowOld).toHaveBeenCalledTimes(4);
+  });
+
+});
+
+
+describe('Class Phoenix extends Essence', () => {
+
+  it('Phoenix instanceof Essence', () => {
+    expect(new Phoenix()).toBeInstanceOf(Essence);
+  });
+
+  it('Check reincarnate property after creating new Phoenix without params', () => {
+    const phoenix = new Phoenix();
+    expect(phoenix.reincarnate).toByTruthy;
+  });
+
+  it('Infinite recursive calling growOld if reincarnate = true', () => {
+    const phoenix = new Phoenix(1);
+    const spyOnGrowOld = jest.spyOn(phoenix, 'growOld');
+    // clear mock
+    spyOnGrowOld.mockClear();
+    // run all timers without delay
+    jest.useFakeTimers();
+
+    phoenix.growOld();
+    // run all timers;
+    jest.advanceTimersByTime(1000);
+    expect(spyOnGrowOld).toHaveBeenCalledTimes(2);
+  });
+
+  it('Stop recursive calling growOld if reincarnate = false', () => {
+    const phoenix = new Phoenix(1, false);
+    const spyOnGrowOld = jest.spyOn(phoenix, 'growOld');
+    // clear mock
+    spyOnGrowOld.mockClear();
+    // run all timers without delay
+    jest.useFakeTimers();
+
+    phoenix.growOld();
+    // run all timers;
+    jest.advanceTimersByTime(2000);
+    expect(spyOnGrowOld).toHaveBeenCalledTimes(1);
   });
 
 });
